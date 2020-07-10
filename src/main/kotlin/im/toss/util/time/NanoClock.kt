1c7a1e28d1e0dd1da9f341bc30ae2c6823bccd56
Jinsung Oh <econquer@toss.im>
2020-07-11 01:01:02 +0900

    #2 fixed clock calibration if error is greater than 0.5ms

--- src/main/kotlin/im/toss/util/time/NanoClock.kt
+++ src/main/kotlin/im/toss/util/time/NanoClock.kt
@@ -3,62 +3,104 @@ package im.toss.util.time
 import java.time.Clock
 import java.time.Instant
 import java.time.ZoneId
+import java.util.concurrent.atomic.AtomicReference
+import kotlin.math.abs
 
 /**
  * NanoClock
  *
  * nanoseconds 해상도를 가지는 시계
  */
-class NanoClock(private val zone: ZoneId = ZoneId.systemDefault()): Clock() {
+class NanoClock(
+    private val clockSource: ClockSource = SystemClockSource,
+    private val zone: ZoneId = ZoneId.systemDefault(),
+    private val synchronizedClockSnapshotReference: AtomicReference<SynchronizedClockSnapshot> = AtomicReference(clockSource.synchronizeWithRealtime(10))
+): Clock() {
     companion object {
-        // 동기화된 시간 샘플 10개 중, 오차가 가장 적을것으로 예상되는 1개를 선택한다
-        private val baseTime = (1..10).map { BaseTime.sync() }.minBy { it.elapsed }!!
         private const val NANOS_PER_MILLI = 1_000_000L
         private const val MILLIS_PER_SEC = 1_000L
 
-        val DEFAULT = NanoClock()
+        val DEFAULT = NanoClock(SystemClockSource)
     }
 
     override fun withZone(zone: ZoneId): Clock {
         if (zone == this.zone) {
             return this
         }
-        return NanoClock(zone)
+        return NanoClock(clockSource, zone, synchronizedClockSnapshotReference)
     }
 
     override fun getZone(): ZoneId = zone
 
     override fun instant(): Instant {
-        val nanosSinceInit = baseTime.nanoTime()
-        val epochMillis = nanosSinceInit / NANOS_PER_MILLI + baseTime.millis
+        val monotonicNanos = clockSource.monotonicNanos()
+        val snapshot = snapshotWithCalibration(monotonicNanos)
+        val nanosSinceBase = monotonicNanos - snapshot.monotonicNanos
+        val epochMillis = nanosSinceBase / NANOS_PER_MILLI + snapshot.realtimeMillis
         val epochSeconds = epochMillis / MILLIS_PER_SEC
-        val nanoAdjustment = epochMillis % MILLIS_PER_SEC * NANOS_PER_MILLI + nanosSinceInit % NANOS_PER_MILLI
+        val nanoAdjustment = epochMillis % MILLIS_PER_SEC * NANOS_PER_MILLI + nanosSinceBase % NANOS_PER_MILLI
         return Instant.ofEpochSecond(epochSeconds, nanoAdjustment)
     }
 
-    private data class BaseTime(
-        val millis: Long,
-        private val nanos: Long,
-        val elapsed: Long
-    ) {
-        fun nanoTime() = System.nanoTime() - nanos
+    private fun snapshotWithCalibration(monotonicNanos: Long): SynchronizedClockSnapshot {
+        val snapshot = synchronizedClockSnapshotReference.get()
+        val realtimeMillis = clockSource.realtimeMillis()
+        val realtimeMillisSinceBase = (realtimeMillis - snapshot.realtimeMillis) * 1_000_000L
+        val nanosSinceBase = monotonicNanos - snapshot.monotonicNanos
+        val diff = abs(nanosSinceBase - realtimeMillisSinceBase - 500_000L)
 
-        companion object {
-            fun sync(): BaseTime {
-                while (true) {
-                    /* 시간 오차를 줄이기 위해 milliseconds 단위의 시간이 변경되는 시점을 측정한다 */
-                    val startNanos = System.nanoTime()
-                    val prevMillis = System.currentTimeMillis()
-                    val millis = System.currentTimeMillis()
-                    val nanos = System.nanoTime()
-                    val elapsedNanos = nanos - startNanos
-                    if (millis - prevMillis == 1L) {
-                        return BaseTime(millis, nanos - elapsedNanos, elapsedNanos)
-                    }
-                }
-            }
+        return if (diff < 1_000_000L) {
+            snapshot
+        } else {
+            calibration()
+        }
+    }
+
+    private fun calibration(): SynchronizedClockSnapshot {
+        val newBaseTime = clockSource.synchronizeWithRealtime(3)
+        synchronizedClockSnapshotReference.set(newBaseTime)
+        return newBaseTime
+    }
+}
+
+data class SynchronizedClockSnapshot(
+    val realtimeMillis: Long,
+    val monotonicNanos: Long,
+    val elapsed: Long
+)
+
+interface ClockSource {
+    fun monotonicNanos(): Long
+    fun realtimeMillis(): Long
+}
+
+object SystemClockSource: ClockSource {
+    override fun monotonicNanos(): Long = System.nanoTime()
+    override fun realtimeMillis(): Long = System.currentTimeMillis()
+}
+
+
+private fun ClockSource.synchronizeWithRealtime(): SynchronizedClockSnapshot {
+    while (true) {
+        val startNanos = monotonicNanos()
+        val millis1 = realtimeMillis()
+        val millis2 = realtimeMillis()
+        val nanos = monotonicNanos()
+        val elapsedNanos = nanos - startNanos
+        if (millis2 - millis1 == 1L) {
+            /* 시간 오차를 줄이기 위해 milliseconds단위의 realtime이 변경되는 시점의 값을 측정한다 */
+            return SynchronizedClockSnapshot(
+                realtimeMillis = millis2,
+                monotonicNanos = nanos - elapsedNanos,
+                elapsed = elapsedNanos
+            )
         }
     }
 }
+
+private fun ClockSource.synchronizeWithRealtime(candidates: Int): SynchronizedClockSnapshot =
+    (1..candidates)
+        .map { synchronizeWithRealtime() }
+        .minBy { it.elapsed }!!
 
 
--- src/test/kotlin/im/toss/util/time/NanoClockTest.kt
+++ src/test/kotlin/im/toss/util/time/NanoClockTest.kt
@@ -1,8 +1,18 @@
 package im.toss.util.time
 
 import im.toss.test.equalsTo
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.data.Offset
+import org.assertj.core.data.Percentage
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
+import java.time.Duration
 import java.time.ZoneId
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicLong
+import kotlin.system.measureNanoTime
 
 internal class NanoClockTest {
     @Test
@@ -12,4 +22,60 @@ internal class NanoClockTest {
         NanoClock().withZone(ZoneId.of("Asia/Seoul")).zone equalsTo ZoneId.of("Asia/Seoul")
         NanoClock().withZone(ZoneId.of("UTC")).zone equalsTo ZoneId.of("UTC")
     }
+
+    @Test
+    fun performanceTest() {
+        val clockSource = OffsetClockSource(SystemClockSource)
+        val clock = NanoClock(clockSource)
+        clock.instant()
+
+        val totalElapsedNanos = AtomicLong()
+        for(i in 1..1_000_000) {
+            if (i % 100_000 == 0) {
+                val prev = clock.instant()
+                clockSource.offsetMillis += 1500
+                val start = System.nanoTime()
+                val after = clock.instant()
+                val diff = after.nano - prev.nano
+
+                val elapsed = (System.nanoTime() - start) / 1_000_000L
+                println("calibration -> $elapsed ms, $prev -> $after -> ${clock.instant()}, $diff")
+            }
+            totalElapsedNanos.addAndGet(
+                measureNanoTime {
+                    clock.instant()
+                }
+            )
+        }
+
+        val elapsed = totalElapsedNanos.get() / 1_000_000
+        println("$elapsed ms in 1,000,000 times")
+    }
+
+    @TestFactory
+    fun calibrationTest(): List<DynamicTest> {
+        NanoClock(SystemClockSource).instant()
+
+        val allowErrorNanos = 1_000_000L // 1 ms
+        return listOf(0L, 100L, 200L, 300L, 500L, 10000L, -100L, -200L, -500L, -10000L)
+            .map { offset ->
+                dynamicTest("시간이 $offset ms 변경 후 측정하면 오차범위 $allowErrorNanos nano 이하로 보정된다") {
+                    val clockSource = OffsetClockSource(SystemClockSource)
+                    val clock = NanoClock(clockSource)
+
+                    val before = clock.instant()
+                    clockSource.offsetMillis = offset
+                    val after = clock.instant()
+
+                    val duration = Duration.between(before, after).toNanos()
+                    println("before=$before, fix $offset ms, after=$after, duration=$duration")
+                    assertThat(duration).isCloseTo(offset * 1_000_000L, Offset.offset(allowErrorNanos)) // 1ms
+                }
+            }
+    }
+}
+
+class OffsetClockSource(private val clockSource: ClockSource): ClockSource by clockSource {
+    var offsetMillis: Long = 0L
+    override fun realtimeMillis(): Long = clockSource.realtimeMillis() + offsetMillis
 }
